e8cfe5a2dd47b28227f5b05d2a39e25e2c227e44
92536 <925367077@qq.com>
2022-08-26 02:07:26 +0800

    1.0

--- pose_align.py
+++ pose_align.py
@@ -82,9 +82,9 @@ def load_poses_from_txt(file_name):
     return poses
 
 
-pose3 = np.loadtxt("gt/pose4.txt")[:, 3::4]
+pose3 = np.loadtxt("gt/pose2.txt")[:, 3::4]
 pose3 = np.transpose(pose3, (1, 0))
-pose3_test = np.loadtxt("result/pose4_test.txt")[:, 3::4]
+pose3_test = np.loadtxt("result/pose2_test.txt")[:, 3::4]
 pose3_test = np.transpose(pose3_test, (1, 0))
 r, t, scale = umeyama_alignment(pose3_test, pose3, True)
 e = angel_trans.rotation2euler(r)
@@ -94,9 +94,9 @@ print(e, t, scale)
 # r = np.array([[9.57241851e-01, -2.88029769e-01, -2.69609328e-02], [2.89164278e-01, 9.49930449e-01, 1.18389870e-01],
 #               [-8.48879594e-03, -1.21123877e-01, 9.92601101e-01]])
 # pose_2
-# r = np.array(
-#     [[9.47843716e-01, -3.18466057e-01, 1.31019666e-02], [3.13217925e-01, 9.38263990e-01, 1.46816947e-01],
-#      [-5.90493177e-02, -1.35055750e-01, 9.89076904e-01]])
+r = np.array(
+    [[9.47843716e-01, -3.18466057e-01, 1.31019666e-02], [3.13217925e-01, 9.38263990e-01, 1.46816947e-01],
+     [-5.90493177e-02, -1.35055750e-01, 9.89076904e-01]])
 
 # pose_5
 # r = np.array(
@@ -108,11 +108,12 @@ print(e, t, scale)
 #      [2.326810e-11, 2.392370e-10, 9.999999e-01]
 #      ])
 # pose_4
-r = np.array(
-    [[-8.35375522e-01, 5.38562801e-01, 1.09990214e-01], [-5.49491341e-01, -8.12962741e-01, -1.92745554e-01],
-     [-1.43876398e-02, -2.21453587e-01, 9.75064769e-01]])
+# r = np.array(
+#     [[-8.35375522e-01, 5.38562801e-01, 1.09990214e-01], [-5.49491341e-01, -8.12962741e-01, -1.92745554e-01],
+#      [-1.43876398e-02, -2.21453587e-01, 9.75064769e-01]])
 delta = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
-r = delta @ r
+delta2 = np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]])
+r = r @ delta @ delta2
 t = np.array([0, 0, 0.00000000e+00])
 align_transformation = np.eye(4)
 align_transformation[:3:, :3] = r
@@ -120,20 +121,20 @@ align_transformation[:3, 3] = t
 
 print(1 / scale)
 
-poses_result = load_poses_from_txt("result/pose4_test.txt")
+poses_result = load_poses_from_txt("result/pose2_test.txt")
 poses = [align_transformation[:3, :].reshape(1, 12)]
 for cnt in poses_result:
     poses_result[cnt][:3, 3] *= scale
-    a = poses_result[cnt][[0, 2, 1, 3], :]
-    pose = align_transformation @ a
+    # a = poses_result[cnt][[0, 2, 1, 3], :]
+    pose = align_transformation @ poses_result[cnt]
     poses.append(pose[:3, :].reshape(1, 12))
 
 poses.pop(1)
 poses = np.concatenate(poses, axis=0)
 # poses_result = np.array(poses_result)
-np.savetxt("result/pose4_te.txt", poses, delimiter=' ', fmt='%1.8e')
+np.savetxt("result/pose2_te.txt", poses, delimiter=' ', fmt='%1.8e')
 
-# evo_traj kitti result/pose4_test.txt --ref gt/pose4.txt -p --plot_mod=xy -as ** Please tell me who you are. Run git
+# evo_traj kitti result/pose2_test.txt --ref gt/pose2.txt -p --plot_mod=xy -as ** Please tell me who you are. Run git
 # config --global user.email "925367077@qq.com" git config --global user.name "wanglong1008" to set your account's
 # default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (
 # got '92536@wl_y9000p.(none)')
